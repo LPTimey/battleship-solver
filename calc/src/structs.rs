d11b68ab4ab5ffbecec441b389af1ffc2865b5c8
LPTimey <tim.ruland@icloud.com>
2023-06-24 17:53:20 +0200

    added data structure

--- calc/src/structs.rs
+++ calc/src/structs.rs
@@ -1,7 +1,48 @@
 #![allow(dead_code)]
 use std::ops::{Add, Mul, Sub};
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+use itertools::Itertools;
+use thiserror::Error;
+
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Pos2 {
+    x: usize,
+    y: usize,
+}
+
+impl Pos2 {
+    pub fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
+    pub fn distance(&self, rhs: &Self) -> Vec2 {
+        Vec2::new(
+            rhs.x as isize - self.x as isize,
+            rhs.y as isize - self.y as isize,
+        )
+    }
+}
+impl Add<Vec2> for Pos2 {
+    type Output = Self;
+
+    fn add(self, rhs: Vec2) -> Self::Output {
+        Self::new(self.x - rhs.x as usize, self.y - rhs.y as usize)
+    }
+}
+#[cfg(test)]
+mod pos2_tests {
+    use crate::structs::Vec2;
+
+    use super::Pos2;
+
+    #[test]
+    fn neg_dist() {
+        let pos1 = Pos2::new(3, 2);
+        let pos2 = Pos2::new(1, 1);
+        assert_eq!(Vec2::new(-2, -1), pos1.distance(&pos2))
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Vec2 {
     x: isize,
     y: isize,
@@ -60,14 +101,23 @@ impl Vec2 {
     pub fn dot_prod(&self, rhs: &Self) -> isize {
         (self.x * rhs.x) + (self.y * rhs.y)
     }
-    pub fn area(&self) -> isize {
+    pub fn area_rect(&self) -> isize {
         self.x * self.y
+    }
+    pub fn area_tri(&self) -> f64 {
+        (self.x * self.y) as f64 / 2.
+    }
+    pub fn turn_counter(self) -> Self {
+        Self::new(-self.y, self.x)
+    }
+    pub fn turn_clock(self) -> Self {
+        Self::new(self.y, -self.x)
     }
 }
 
 #[cfg(test)]
 mod vec2_tests {
-    use super::*;
+    use super::Vec2;
 
     #[test]
     fn add_vec2s() {
@@ -102,28 +152,156 @@ mod vec2_tests {
         let dot = vec1.dot_prod(&vec2);
         assert_eq!(dot, 18);
     }
+    #[test]
+    fn turn_clock() {
+        let vec = Vec2::new(2, 1);
+        assert_eq!(Vec2::new(1, -2), vec.turn_clock());
+        assert_eq!(Vec2::new(-2, -1), vec.turn_clock().turn_clock());
+        assert_eq!(Vec2::new(-1, 2), vec.turn_clock().turn_clock().turn_clock());
+    }
+    #[test]
+    fn turn_counter_clock() {
+        let vec = Vec2::new(2, 1);
+        assert_eq!(Vec2::new(-1, 2), vec.turn_counter());
+        assert_eq!(Vec2::new(-2, -1), vec.turn_counter().turn_counter());
+        assert_eq!(
+            Vec2::new(1, -2),
+            vec.turn_counter().turn_counter().turn_counter()
+        );
+    }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Ship {
     shape: Vec<Vec2>,
-    pos: Vec2,
+}
+
+#[derive(Debug, Error)]
+pub enum ShipError {
+    #[error("there needs to be a RootVector(Vec2{{0,0}})")]
+    NoRootVec,
 }
 
 impl Ship {
-    pub fn new(shape: Vec<Vec2>, pos: Vec2) -> Self {
-        Self { shape, pos }
+    pub fn new(mut shape: Vec<Vec2>) -> Result<Self, ShipError> {
+        if !shape.contains(&Vec2::new(0, 0)) {
+            shape.push(Vec2::new(0, 0));
+            //return Err(ShipError::NoRootVec);
+        };
+        let shape = shape.into_iter().unique().collect_vec();
+        Ok(Self { shape })
+    }
+    pub fn get_new(shape: Vec<Vec2>) -> Result<[Self; 4], ShipError> {
+        let shape = shape.into_iter().unique().collect_vec();
+        let norm: Self = Self::new(shape.clone())?;
+        let r90: Self = Self::new(
+            shape
+                .clone()
+                .into_iter()
+                .map(|vec| vec.turn_counter())
+                .collect(),
+        )?;
+        let r180: Self = Self::new(
+            shape
+                .clone()
+                .into_iter()
+                .map(|vec| vec.turn_counter().turn_counter())
+                .collect(),
+        )?;
+        let r270: Self = Self::new(
+            shape
+                .into_iter()
+                .map(|vec| vec.turn_counter().turn_counter().turn_counter())
+                .collect(),
+        )?;
+        Ok([norm, r180, r90, r270])
+    }
+    pub fn is_equivalent(&self, other: &Self) -> bool {
+        let (Ship { shape }, Ship { shape: other }) = (self, other);
+        if shape.len() != other.len() {
+            return false;
+        }
+        let mut res: bool = true;
+        shape.iter().for_each(|vec| {
+            if !other.contains(vec) {
+                res = false;
+            }
+        });
+        res
+    }
+}
+
+#[cfg(test)]
+mod ship_tests {
+    use itertools::Itertools;
+
+    use super::{Ship, Vec2};
+
+    #[test]
+    fn test() {
+        let ships = Ship::get_new(vec![
+            Vec2::default(),
+            Vec2::new(1, 0),
+            Vec2::new(0, 1),
+            Vec2::new(-1, 0),
+            Vec2::new(0, -1),
+        ])
+        .unwrap();
     }
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord, Hash)]
 pub struct Board {
-    ships: Vec<Ship>,
-    size: Vec2,
+    ships: Vec<(Pos2, Ship)>,
+    shape: Vec<Pos2>,
+    shots: Vec<Pos2>,
 }
+#[derive(Debug, Error)]
+pub enum BoardErrors {}
 
 impl Board {
-    pub fn new(ships: Vec<Ship>, size: Vec2) -> Self {
-        Self { ships, size }
+    pub fn new(
+        ships: Vec<(Pos2, Ship)>,
+        shape: Vec<Pos2>,
+        shots: Vec<Pos2>,
+    ) -> Result<Self, BoardErrors> {
+        Ok(Self {
+            ships,
+            shape,
+            shots,
+        })
+    }
+
+    pub fn size(&self) -> Vec2 {
+        let mut max_x = 0;
+        let mut max_y = 0;
+        self.shape.iter().for_each(|Pos2 { x, y }| {
+            if x > &max_x {
+                max_x = *x;
+            }
+            if y > &max_y {
+                max_y = *y;
+            }
+        });
+        Vec2::new(max_x as isize, max_y as isize)
+    }
+    pub fn get_grid(&self) -> Vec<Pos2>{
+        self.ships
+            .clone()
+            .into_iter()
+            .flat_map(|(pos, Ship { shape })| {
+                shape.into_iter().map(move |vec| pos + vec)
+            }).collect_vec()
+    }
+    pub fn get_completion(&self) -> f64 {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod board_tests {
+    #[test]
+    fn test() {
+        assert!(true)
     }
 }
---
+++ gui/notes/ui stuff.md
@@ -0,0 +1,3 @@
+# Stuff for ui
+
+<https://www.youtube.com/watch?v=88XxC0_zs74&t=290s>
